e9b5564209646bd7509a65faffb214c5adc93a14
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-22 05:07:03 +0530

    Adding project Skeleton

---
+++ src/main/java/com/phonpe/cabmanagement/CabmanagementApplication.java
@@ -0,0 +1,15 @@
+package com.phonpe.cabmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabmanagementApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CabmanagementApplication.class, args);
+    }
+
+}
