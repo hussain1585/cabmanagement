64d949bb9de0148f43f0f60defcd0c9b842fe290
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-22 22:19:15 +0530

    Adding skeleton service for cabs

--- src/main/java/com/phonpe/cabmanagement/service/CabService.java
+++ src/main/java/com/phonpe/cabmanagement/service/CabService.java
@@ -1,2 +1,16 @@
-package com.phonpe.cabmanagement.service;public class CabService {
+package com.phonpe.cabmanagement.service;
+
+import com.phonpe.cabmanagement.domain.Cab;
+import com.phonpe.cabmanagement.dto.GetNearByCabsRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CabService
+{
+    public Set<Cab> getNearByCabs(GetNearByCabsRequest getNearByCabsRequest)
+    {
+        return null;
+    }
 }
