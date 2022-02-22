668842427b2a12aa863e62d73aa8a000957ab5aa
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-22 22:18:47 +0530

    Adding Skeleton for rest apis controller

--- src/main/java/com/phonpe/cabmanagement/rest/CabController.java
+++ src/main/java/com/phonpe/cabmanagement/rest/CabController.java
@@ -1,8 +1,28 @@
 package com.phonpe.cabmanagement.rest;
 
+import com.phonpe.cabmanagement.domain.Cab;
+import com.phonpe.cabmanagement.dto.GetNearByCabsRequest;
+import com.phonpe.cabmanagement.service.CabService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
+@Slf4j
 public class CabController
 {
+
+    @Autowired
+    private CabService cabService;
+
+    @PostMapping
+    public Set<Cab> getNearByCabs(@RequestBody GetNearByCabsRequest getNearByCabsRequest)
+    {
+        log.info("fetching list of near by cabs for location => {}", getNearByCabsRequest.getLocation());
+        return cabService.getNearByCabs(getNearByCabsRequest);
+    }
 }
