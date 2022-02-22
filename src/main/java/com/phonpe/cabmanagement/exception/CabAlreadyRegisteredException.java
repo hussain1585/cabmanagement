27862aa52f90c6a6aefefd3c7939b364044d23f3
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-23 05:17:33 +0530

    Adding custom exceptions for exisiting user and rider registration'

--- src/main/java/com/phonpe/cabmanagement/exception/CabAlreadyRegisteredException.java
+++ src/main/java/com/phonpe/cabmanagement/exception/CabAlreadyRegisteredException.java
@@ -1,2 +1,13 @@
-package com.phonpe.cabmanagement.exception;public class CabAlreadyRegisteredException {
+package com.phonpe.cabmanagement.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CabAlreadyRegisteredException extends RuntimeException
+{
+    public CabAlreadyRegisteredException(String message)
+    {
+        super(message);
+        log.error("CabAlreadyRegisteredException with error : {}", message);
+    }
 }
--- src/main/java/com/phonpe/cabmanagement/exception/RiderAlreadyRegisteredException.java
+++ src/main/java/com/phonpe/cabmanagement/exception/RiderAlreadyRegisteredException.java
@@ -1,2 +1,13 @@
-package com.phonpe.cabmanagement.exception;public class RiderAlreadyRegisteredException {
+package com.phonpe.cabmanagement.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RiderAlreadyRegisteredException extends RuntimeException
+{
+    public RiderAlreadyRegisteredException(String message)
+    {
+        super(message);
+        log.error("RiderAlreadyRegisteredException with error : {}", message);
+    }
 }
