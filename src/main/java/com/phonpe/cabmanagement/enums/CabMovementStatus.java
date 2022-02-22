3a3d8ffbc882cba3a8c1ade3f3301e67d23e4c40
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-23 02:11:23 +0530

    Adding description in moving status enum

--- src/main/java/com/phonpe/cabmanagement/enums/CabMovementStatus.java
+++ src/main/java/com/phonpe/cabmanagement/enums/CabMovementStatus.java
@@ -2,6 +2,7 @@ package com.phonpe.cabmanagement.enums;
 
 public enum CabMovementStatus
 {
-    IDLE,
-    ON_TRIP;
+    IDLE, // Driver is available for booking
+    OFFLINE, // Driver can not be reached by servers
+    ON_TRIP; // driver is already serving a trip
 }
