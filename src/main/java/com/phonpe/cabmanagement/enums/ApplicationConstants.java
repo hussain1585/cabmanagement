2d0755e2bd411e5667e8b6e49ed6ee2fe86beec9
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-23 05:21:08 +0530

    Adding applciation constants num

--- src/main/java/com/phonpe/cabmanagement/enums/ApplicationConstants.java
+++ src/main/java/com/phonpe/cabmanagement/enums/ApplicationConstants.java
@@ -1,2 +1,19 @@
-package com.phonpe.cabmanagement.enums;public class ApplicationConstants {
+package com.phonpe.cabmanagement.enums;
+
+public enum ApplicationConstants
+{
+    SUCCESS("SUCCESS"),
+    FAILURE("FAILURE");
+
+    private String value;
+
+    ApplicationConstants(String value)
+    {
+        this.value = value;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
 }
