76a0f4f77786bc8e9c1053f42c88650d30e84d60
Hussain Akhtar Wahid <hussain@chegg.com>
2022-02-22 22:15:59 +0530

    Adding DTO for getting cabs in proximity

--- src/main/java/com/phonpe/cabmanagement/dto/GetNearByCabsRequest.java
+++ src/main/java/com/phonpe/cabmanagement/dto/GetNearByCabsRequest.java
@@ -1,2 +1,12 @@
-package com.phonpe.cabmanagement.dto;public class GetNearByCabsRequest {
+package com.phonpe.cabmanagement.dto;
+
+import com.phonpe.cabmanagement.domain.Location;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class GetNearByCabsRequest
+{
+    private Location location;
 }
